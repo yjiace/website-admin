fdf2008c2e47fe54e2aff2abc55aac8149dd51eb
yjc <504015995@qq.com>
2020-12-17 20:55:31 +0800

    行为验证码

---
+++ src/main/java/cn/smallyoung/websiteadmin/controller/sys/LoginController.java
@@ -0,0 +1,76 @@
+package cn.smallyoung.websiteadmin.controller.sys;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.CircleCaptcha;
+import cn.hutool.core.lang.Dict;
+import cn.hutool.core.util.StrUtil;
+import cn.smallyoung.websiteadmin.interfaces.ResponseResultBody;
+import cn.smallyoung.websiteadmin.service.sys.SysUserService;
+import com.anji.captcha.model.common.ResponseModel;
+import com.anji.captcha.model.vo.CaptchaVO;
+import com.anji.captcha.service.CaptchaService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author smallyoung
+ * @date 2020/10/26
+ */
+@Slf4j
+@RestController
+@ResponseResultBody
+public class LoginController {
+
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+
+    @Resource
+    private CaptchaService captchaService;
+    @Resource
+    private SysUserService sysUserService;
+
+    private static final String CHAPTER_KEY = "login-captcha";
+
+    /**
+     * 登录
+     *
+     * @param username 用户名
+     * @param password 密码
+     * @return token
+     */
+    @GetMapping("/login")
+    public Dict login(String username, String password, String captchaVerification) {
+        if (StrUtil.hasBlank(username, password, captchaVerification)) {
+            throw new NullPointerException("参数错误");
+        }
+        CaptchaVO captchaVO = new CaptchaVO();
+        captchaVO.setCaptchaVerification(captchaVerification);
+        ResponseModel responseModel = captchaService.verification(captchaVO);
+        if(!responseModel.isSuccess()){
+            log.error("用户【{}】登录系统，验证码【{}】错误，{}", username, captchaVerification, responseModel);
+            throw new NullPointerException(responseModel.getRepMsg());
+        }
+        String token = sysUserService.login(username, password);
+        log.info("用户【{}】登录系统", username);
+        return Dict.create().set("tokenHead", tokenHead).set("token", token).set("username", username);
+    }
+
+    @GetMapping("/captcha")
+    public void captcha(HttpSession session, HttpServletResponse response) throws IOException {
+        response.setContentType("image/png");
+        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(200, 100, 4, 20);
+        session.setAttribute(CHAPTER_KEY, captcha.getCode());
+        OutputStream os = response.getOutputStream();
+        captcha.write(os);
+        os.flush();
+        os.close();
+    }
+}
---
+++ src/main/java/cn/smallyoung/websiteadmin/service/CaptchaCacheServiceRedis.java
@@ -0,0 +1,40 @@
+package cn.smallyoung.websiteadmin.service;
+
+import com.anji.captcha.service.CaptchaCacheService;
+import com.google.auto.service.AutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@AutoService(CaptchaCacheService.class)
+public class CaptchaCacheServiceRedis implements CaptchaCacheService {
+
+    @Override
+    public String type() {
+        return "redis";
+    }
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void set(String key, String value, long expiresInSeconds) {
+        stringRedisTemplate.opsForValue().set(key, value, expiresInSeconds, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public boolean exists(String key) {
+        return stringRedisTemplate.hasKey(key);
+    }
+
+    @Override
+    public void delete(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    @Override
+    public String get(String key) {
+        return stringRedisTemplate.opsForValue().get(key);
+    }
+}
