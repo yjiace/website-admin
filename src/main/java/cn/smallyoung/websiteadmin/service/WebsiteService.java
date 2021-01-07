1725568d96a1f08a968b49f00cc1bc004eda9794
yjc <504015995@qq.com>
2021-01-07 08:15:49 +0800

    静态化方法完善

--- src/main/java/cn/smallyoung/websiteadmin/service/WebsiteService.java
+++ src/main/java/cn/smallyoung/websiteadmin/service/WebsiteService.java
@@ -1,17 +1,22 @@
 package cn.smallyoung.websiteadmin.service;
 
 import cn.hutool.core.io.file.FileWriter;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.template.Template;
 import cn.hutool.extra.template.TemplateConfig;
 import cn.hutool.extra.template.TemplateEngine;
 import cn.hutool.extra.template.TemplateUtil;
 import cn.hutool.json.JSONObject;
 import cn.smallyoung.websiteadmin.base.BaseService;
+import cn.smallyoung.websiteadmin.entity.Category;
 import cn.smallyoung.websiteadmin.entity.Website;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.stream.Collectors;
 
 /**
  * @author smallyoung
@@ -24,12 +29,37 @@ public class WebsiteService extends BaseService<Website, String> {
 
     @Value("${website.catalog}")
     private String dirPath;
+    @Resource
+    private ArticleService articleService;
+    @Resource
+    private CategoryService categoryService;
 
     public void staticWebsite(Website website){
         TemplateEngine engine = TemplateUtil.createEngine(new TemplateConfig("templates", TemplateConfig.ResourceMode.CLASSPATH));
         Template template = engine.getTemplate(website.getFileName());
-        String result = template.render(new JSONObject().getJSONObject(website.getData()));
+        String result = template.render(this.getData(website.getKey(), website.getData()));
         FileWriter writer = new FileWriter(dirPath + website.getFileName(), "UTF-8");
         writer.write(result);
+    }
+
+
+    private JSONObject getData(String key, String data){
+        JSONObject jsonObject = new JSONObject();
+        if(StrUtil.isNotBlank(data)){
+            jsonObject.getJSONObject(data);
+        }
+        if(StrUtil.isNotBlank(key)){
+            switch (key){
+                case "index":
+                    jsonObject.set("recommend", articleService.findRecommendArticle());
+                    break;
+                case "blog":
+                    jsonObject.set("categories", categoryService.findAll().stream().map(Category::toMap).collect(Collectors.toList()));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return jsonObject;
     }
 }
